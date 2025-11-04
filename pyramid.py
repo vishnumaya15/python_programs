d8f8a4cc51ce838575db496812bdddd1787ffc39
Vishnumaya <vishnumaya@vvdntech.in>
2025-11-04 12:23:11 +0530

    pyramid changes

--- pyramid.py
+++ pyramid.py
@@ -6,3 +6,8 @@ for i in range(1,rows+1):
 
 for i in range(rows,0,-1):
     print(" " * (rows-i)+ "*"*(2*i-1))
+
+for i in range (rows+1):
+    for j in range (i):
+        print("*",end ="")
+    print()
