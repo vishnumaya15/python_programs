0b876dfe25d31440e8ea386b421db2f25ab70b4c
Vishnumaya <vishnumaya6316@gmail.com>
2025-11-05 20:14:20 +0530

    COmmit

--- pyramid.py
+++ pyramid.py
@@ -6,3 +6,12 @@ for i in range(1,rows+1):
 
 for i in range(rows,0,-1):
     print(" " * (rows-i)+ "*"*(2*i-1))
+
+
+for i in range(1, rows + 1):           # Outer loop → rows
+    for j in range(1, 2 * rows):       # Inner loop → columns
+        if j == rows - i + 1 or j == rows + i - 1 or i == rows:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
