22f90963626e7d040b22dec89fe551bbd4008f71
Vishnumaya <vishnumaya6316@gmail.com>
2025-11-11 21:45:17 +0530

    Commit

--- reverse.py
+++ reverse.py
@@ -6,12 +6,16 @@ print(" ".join(reversed(test.split())))
 
 print(test[::-1])
 
-reversed = ""
+reverse = ""
 for char in test:
-    reversed = char + reversed
-print ("Reverse",reversed)
+    reverse = char + reverse
+print ("Reverse",reverse)
 
 test1="Vishnu"
 print(test1[0]+test1[1:-1][::-1]+test1[-1])
 
 print(" ".join(test.split()[::-1]))
+
+
+print(" ".join(test.split()[::-1]))
+print(" ".join(reversed(test.split())))
